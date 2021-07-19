d32f0c684267aa72569549caa34ab7868c65a9a9
somji <somji@Somjit8>
2021-07-18 20:42:46 -0400

    First Project

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main (String[] args) {
+		
+		System.out.println("Welcome to the World of Java!");
+		System.out.println("Jave Project to the Github");
+		
+	}
+
+}
